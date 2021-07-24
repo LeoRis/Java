faf73a48a3bc754e35a6f2fc46f59b4846bd2a83
LeoRis <ristanovski@hotmail.com>
2021-07-24 19:30:04 +0200

    Delete Student.java

--- Student.java
+++
@@ -1,8 +0,0 @@
-public class Student extends User{
-	public String major;
-	
-	@Override
-	public void sayHello() {
-		System.out.println("I am " + firstName + " " + lastName + ". My major is " + major + ".");
-	}
-}
