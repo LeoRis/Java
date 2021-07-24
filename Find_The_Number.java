7c0e73d1970e222fb2ea43cdddbf2baf6c875857
LeoRis <ristanovski@hotmail.com>
2021-07-24 19:12:22 +0200

    Update Find_The_Number.java

--- Find_The_Number.java
+++ Find_The_Number.java
@@ -32,3 +32,292 @@ public class Find_The_Number {
     }
    
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Implements an unbalanced binary search tree.
+ * Note that all "matching" is based on the compareTo method.
+ * @author Mark Allen Weiss
+ */
+public class Binary_Search_Tree<E extends Comparable<E>> {
+    
+    /** The tree root. */
+    public BNode<E> root;
+    public int[] array;
+    public int size;
+    
+    /**
+     * Construct the tree.
+     */
+    public Binary_Search_Tree(int N) {
+        root = null;
+        this.size = N;
+        this.array = new int[this.size];
+    }
+    /**
+     * Count nodes
+     */
+    public int countNode(BNode<E> root){
+        //base case
+        if(root == null)
+            return 0;
+
+        //recursive call to left child and right child and
+        // add the result of these with 1 ( 1 for counting the root)
+        return 1 + countNode(root.left) + countNode(root.right);
+    }
+    
+    /**
+     * Insert into the tree; duplicates are ignored.
+     * @param x the item to insert.
+     */
+    public void insert(E x) {
+        root = insert(x, root);
+    }
+
+    /**
+     * Remove from the tree. Nothing is done if x is not found.
+     * @param x the item to remove.
+     */
+    public void remove(E x) {
+        root = remove(x, root);
+    }
+
+    /**
+     * Find the smallest item in the tree.
+     * @return smallest item or null if empty.
+     */
+    public E findMin() {
+        return elementAt(findMin(root));
+    }
+
+    /**
+     * Find the largest item in the tree.
+     * @return the largest item of null if empty.
+     */
+    public E findMax() {
+        return elementAt(findMax(root));
+    }
+
+    /**
+     * Find an item in the tree.
+     * @param x the item to search for.
+     * @return the matching item or null if not found.
+     */
+    public BNode<E> find(E x) {
+        return find(x, root);
+    }
+
+    /**
+     * Make the tree logically empty.
+     */
+    public void makeEmpty() {
+        root = null;
+    }
+
+    /**
+     * Test if the tree is logically empty.
+     * @return true if empty, false otherwise.
+     */
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    /**
+     * Print the tree contents in sorted order.
+     */
+    public void printTree() {
+        if (isEmpty()) {
+            System.out.println("Empty tree");
+        } else {
+            printTree(root);
+        }
+    }
+
+    /**
+     * Internal method to get element field.
+     * @param t the node.
+     * @return the element field or null if t is null.
+     */
+    private E elementAt(BNode<E> t) {
+        if (t == null)
+            return null;
+        return t.info;
+    }
+
+    /**
+     * Internal method to insert into a subtree.
+     * @param x the item to insert.
+     * @param t the node that roots the tree.
+     * @return the new root.
+     */
+    private BNode<E> insert(E x, BNode<E> t) {
+        if (t == null) {
+            t = new BNode<E>(x, null, null);
+        } else if (x.compareTo(t.info) < 0) {
+            t.left = insert(x, t.left);
+        } else if (x.compareTo(t.info) > 0) {
+            t.right = insert(x, t.right);
+        } else;  // Duplicate; do nothing
+        return t;
+    }
+
+    /**
+     * Internal method to remove from a subtree.
+     * @param x the item to remove.
+     * @param t the node that roots the tree.
+     * @return the new root.
+     */
+    private BNode<E> remove(Comparable x, BNode<E> t) {
+        if (t == null)
+            return t;   // Item not found; do nothing
+        
+        if (x.compareTo(t.info) < 0) {
+            t.left = remove(x, t.left);
+        } else if (x.compareTo(t.info) > 0) {
+            t.right = remove(x, t.right);
+        } else if (t.left != null && t.right != null) { // Two children
+            t.info = findMin(t.right).info;
+            t.right = remove(t.info, t.right);
+        } else {
+            if (t.left != null)
+                return t.left;
+            else
+                return t.right;
+        }
+        return t;
+    }
+
+    /**
+     * Internal method to find the smallest item in a subtree.
+     * @param t the node that roots the tree.
+     * @return node containing the smallest item.
+     */
+    private BNode<E> findMin(BNode<E> t) {
+        if (t == null) {
+            return null;
+        } else if (t.left == null) {
+            return t;
+        }
+        return findMin(t.left);
+    }
+
+    /**
+     * Internal method to find the largest item in a subtree.
+     * @param t the node that roots the tree.
+     * @return node containing the largest item.
+     */
+    private BNode<E> findMax(BNode<E> t) {
+        if (t == null) {
+            return null;
+        } else if (t.right == null) {
+            return t;
+        }
+        return findMax(t.right);
+    }
+
+    /**
+     * Internal method to find an item in a subtree.
+     * @param x is item to search for.
+     * @param t the node that roots the tree.
+     * @return node containing the matched item.
+     */
+    private BNode<E> find(E x, BNode<E> t) {
+        if (t == null)
+            return null;
+        
+        if (x.compareTo(t.info) < 0) {
+            return find(x, t.left);
+        } else if (x.compareTo(t.info) > 0) {
+            return find(x, t.right);
+        } else {
+            return t;    // Match
+        }
+    }
+
+    /**
+     * Internal method to print a subtree in sorted order.
+     * @param t the node that roots the tree.
+     */
+    private void printTree(BNode<E> t) {
+        if (t != null) {
+            printTree(t.left);
+            System.out.println(t.info);
+            printTree(t.right);
+        }
+    }
+    
+
+	int i = 0;
+    /**
+     * Using InOrder traversal method 
+     * @param n
+     */
+	public void inOrder(BNode<E> n) {
+		if(n != null) {
+			inOrder(n.left);
+			array[i] = (int) n.info;
+			System.out.println(n.info);
+			inOrder(n.right);
+			array[i] = (int) n.info;
+			i++;
+		}
+	}
+	
+	/**
+	 * Reversed InOrder
+	 */
+	public void reversedInOrder(BNode<E> n) {
+		if(n != null) {
+			reversedInOrder(n.right);
+			array[i] = (int) n.info;
+			System.out.println(n.info);
+			reversedInOrder(n.left);
+			array[i] = (int) n.info;
+			i++;
+		}
+	}
+	
+	/**
+	 * Sorting array
+	 */
+	
+	public void sortArray() {
+		int temp = 0;
+		for(int i = 0; i < array.length - 1; i++) {
+			for(int j = i + 1; j < array.length; j++) {
+				if(array[j] < array[i]) {
+					temp = array[j];
+					array[j] = array[i];
+					array[i] = temp;
+				}
+			}
+		}
+	}
+    
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public class BNode<E> {
+	public E info;
+	public BNode<E> left;
+	public BNode<E> right;
+	
+	static int LEFT = 1;
+	static int RIGHT = 2;
+	
+	public BNode(E info) {
+		this.info = info;
+		left = null;
+		right = null;
+	}
+	
+	public BNode(E info, BNode<E> left, BNode<E> right) {
+		this.info = info;
+		this.left = left;
+		this.right = right;
+	}
+	
+}
