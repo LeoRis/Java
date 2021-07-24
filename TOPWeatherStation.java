26fb023f67b4381f697d165f8590a88a57cac49a
LeoRis <ristanovski@hotmail.com>
2021-07-24 19:31:50 +0200

    Update TOPWeatherStation.java

--- TOPWeatherStation.java
+++ TOPWeatherStation.java
@@ -55,3 +55,79 @@ public class TOPWeatherStation {
 	}
 
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+
+public class Measurement implements Comparable<Measurement> {
+	float temperature;
+	float wind;
+	float humidity;
+	float visibility;
+	LocalDateTime date;
+	
+	public Measurement(float temperature, float wind, float humidity, float visibility, LocalDateTime date) {
+		this.temperature = temperature;
+		this.wind = wind;
+		this.humidity = humidity;
+		this.visibility = visibility;
+		this.date = date;
+	}
+	
+	@Override
+	public int compareTo(Measurement other) {
+		long span = Math.abs(other.date.until(date, ChronoUnit.SECONDS));
+		if(span < 150) {
+			return 0;
+		}
+		else {
+			return date.compareTo(other.date);
+		}
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%.1f %.1f km/h %.1f%% %.1f km %s", temperature, wind, humidity, visibility, date);
+	}
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.time.LocalDateTime;
+import java.util.TreeSet;
+import java.util.function.Predicate;
+
+public class WeatherStation {
+	TreeSet<Measurement> measurements;
+	private int days;
+	
+	public WeatherStation(int days) {
+		this.days = days;
+		this.measurements = new TreeSet<>();
+	}
+	
+	public void addMeasurement(float temperature, float wind, float humidity, float visibility, LocalDateTime date) {
+		Measurement m = new Measurement(temperature, wind, humidity, visibility, date);
+		if(!measurements.add(m)) {
+			return ;
+		}
+		
+		Predicate<Measurement> old = measurement -> (measurement.date.minusDays(days).isAfter(date));
+		measurements.removeIf(old);
+	}
+	
+	public int total() {
+		return measurements.size();
+	}
+	
+	public void status(LocalDateTime from, LocalDateTime to) {
+		Predicate<Measurement> isInRange = measurement -> !(measurement.date.isBefore(from)) || measurement.date.isAfter(to);
+		
+		double averageTemperature = measurements.stream().filter(isInRange).mapToDouble(measurement -> measurement.temperature).average().orElse(0);
+		measurements.stream().filter(isInRange).forEach(System.out::println);
+		
+		System.out.printf("Average temperature: %.2f\n", averageTemperature);
+	}
+}
