2ccf2e303b07b5add48e79086e80239544b7cfff
LeoRis <ristanovski@hotmail.com>
2021-07-25 18:33:19 +0200

    Update MainApp.java

--- GitHub/MainApp.java
+++ GitHub/MainApp.java
@@ -27,3 +27,105 @@ public class MainApp {
 	}
 
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+package GitHub;
+
+public abstract class MobileApp implements Cloneable {
+	protected String name;
+	protected int[] usersRating;
+	
+	public MobileApp(String name, int[] usersRating) {
+		this.name = name;
+		this.usersRating = usersRating;
+	}
+	
+	public abstract double averageRating();
+	
+	public abstract double rating();
+	
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj) {
+			return true;
+		}
+		if(obj == null) {
+			return false;
+		}
+		
+		MobileApp other = (MobileApp)obj;
+		
+		if(this.usersRating.length != other.usersRating.length) {
+			return false;
+		}
+		return true;
+	}
+
+		
+	
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		MobileApp cloned = (MobileApp)super.clone();
+		cloned.name = name;
+		cloned.usersRating = usersRating;
+		return cloned;
+	}
+
+	@Override
+	public String toString() {
+		return "The name of the mobile app is: " + this.name + ", and it's average rating is: " + this.averageRating();
+	}
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+package GitHub;
+
+public class MobileAppTool extends MobileApp {
+	private String toolType;
+	
+	public MobileAppTool(String name, int[] usersRating, String toolType) {
+		super(name, usersRating);
+		this.toolType = toolType;
+	}
+	
+	public double averageRating() {
+		double sum = 0.0;
+		for(int i = 0; i < usersRating.length; i++) {
+			sum += usersRating[i];
+		}
+		return sum / usersRating.length;
+	}
+	
+	public double rating() {
+		return this.averageRating();
+	}
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+package GitHub;
+
+public class MobileAppGame extends MobileApp {
+	private int levels;
+	
+	public MobileAppGame(String name, int[] usersRating, int levels) {
+		super(name, usersRating);
+		this.levels = levels;
+	}
+	
+	public double averageRating() {
+		double sum = 0.0;
+		for(int i = 0; i < usersRating.length; i++) {
+			sum += usersRating[i];
+		}
+		return sum / usersRating.length;
+	}
+	
+	public double rating() {
+		return this.averageRating() * this.levels;
+	}
+}
