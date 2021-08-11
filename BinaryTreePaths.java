01f5a275dd983ccb319a0b26556c57a08c610873
LeoRis <ristanovski@hotmail.com>
2021-08-11 18:48:54 +0200

    Update BinaryTreePaths.java

--- BinaryTreePaths.java
+++ BinaryTreePaths.java
@@ -46,3 +46,13 @@ public class BinaryTreePaths {
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+public class TreeNode {
+	int data;
+	TreeNode left;
+	TreeNode right;
+	
+	public TreeNode(int data) {
+		this.data = data;
+	}
+}
